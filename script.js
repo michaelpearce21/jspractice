d0087992e919c619f71e3b2f2ac12a2e09cff5c8
Michael Pearce <michaelpearce@Michaels-Air.broadband>
2021-07-21 08:03:49 +0100

    Walkthrough finished

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 // Wait until the DOM has loaded before running the game
 // Get the button elements, and add event listeners to them
 
@@ -23,6 +24,8 @@ document.addEventListener("DOMContentLoaded", function () {
 function runGame(gameType) {
 
     // Creates two numbers with a value of between 1 and 25
+    document.getElementById("answer-box").value = "";
+    document.getElementById("answer-box").focus();
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
 
@@ -31,6 +34,12 @@ function runGame(gameType) {
 
     if (gameType === "addition") {
         displayAdditionQuestion(num1, num2);
+    }
+    else if (gameType === "multiply") {
+        displayMultiplyQuestion(num1, num2);
+    } 
+    else if (gameType === "subtract") {
+        displaySubtractQuestion(num1, num2);
     } else {
         alert(`Unknown game type: ${gameType}`);
         throw `Unknown game type ${gameType}, aborting`;
@@ -48,11 +57,18 @@ function checkAnswer() {
 
     if (isCorrect) {
         alert("Hey! You got it right :D");
+        incrementScore();
     }
     else {
         alert(`Awww...you answered ${userAnswer}, the correct answer was ${calculatedAnswer[0]} :(`);
+        incrementWrongAnswer();
     }
 
+    document.getElementById("answer-box").addEventListener("keydown", function (event) {
+        if (event.key == "Enter") {
+            checkAnswer();
+        }
+    })
     runGame(calculatedAnswer[1]);
 }
 
@@ -68,6 +84,12 @@ function calculateRightAnswer() {
     if (operator === "+") { // This is the addition game
         return [operand1 + operand2, "addition"]; // return an array containing the correct answer and game type
     }
+    else if (operator === "x") { // This is the subtraction game
+        return [operand1 * operand2, "multiply"]; // return an array containing the correct answer and game type
+    }
+    else if (operator === "-") { // This is the subtraction game
+        return [operand1 - operand2, "subtract"]; // return an array containing the correct answer and game type
+    }
     else {
         alert(`Unimplemented operator: ${operator}`);
         throw `Unimplemented operator ${operator}, aborting`;
@@ -75,11 +97,17 @@ function calculateRightAnswer() {
 }
 
 function incrementScore() {
+    // Gets the current score from the DOM and increments it
 
+    let oldScore = parseInt(document.getElementById("score").innerText);
+    document.getElementById("score").innerText = ++oldScore;
 }
 
 function incrementWrongAnswer() {
+    // Gets the current tally of incorrect answers from the DOM and increments it
 
+    let oldScore = parseInt(document.getElementById("incorrect").innerText);
+    document.getElementById("incorrect").innerText = ++oldScore;
 }
 
 // Displays the questions.
@@ -90,10 +118,14 @@ function displayAdditionQuestion(operand1, operand2) {
     document.getElementById("operator").textContent = "+";
 }
 
-function displaySubtractQuestion() {
-
+function displaySubtractQuestion(operand1, operand2) {
+    document.getElementById("operand1").textContent = operand1 > operand2 ? operand1 : operand2;
+    document.getElementById("operand2").textContent = operand1 > operand2 ? operand2 : operand1;
+    document.getElementById("operator").textContent = "-";
 }
 
-function displayMultiplyQuestion() {
-
+function displayMultiplyQuestion(operand1, operand2) {
+    document.getElementById("operand1").textContent = operand1;
+    document.getElementById("operand2").textContent = operand2;
+    document.getElementById("operator").textContent = "x";
 }
